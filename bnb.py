fd5ceed3bbe80cec7b0592b889be7ade3bf55f9f
Sohail <117794471+Sohail2311@users.noreply.github.com>
2023-05-27 20:12:46 +0530

    Add files via upload

--- bnb.py
+++ bnb.py
@@ -4,7 +4,7 @@ from telethon import TelegramClient
 api_id = '28698295'
 api_hash = '0c2dc742b7f12a21371374d4e6cc1780'
 phone = '+919021131951'
-message = 🎁 Bonus'
+message = '🎁 Bonus'
 username = '@Free_Bnb_Fast_Pay_Bot'
 
 client = TelegramClient('session_name', api_id, api_hash)
